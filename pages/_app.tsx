92cebbd020e99f213cacffc3f1833443ecbc111e
Walter Alejandro Díaz Portillo <wdiazportillo@shutterstock.com>
2023-05-21 20:17:31 +0200

    + Use Roboto font + Change app logo + Test manifest colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,20 +5,31 @@ import Meta from "@/components/meta";
 import "@/styles/globals.css";
 import "../extends";
 import TransactionModal from "@/components/Transaction/Modal";
+import { Roboto } from "next/font/google";
+
+// Roboto, Poppins, Montserrat, Rubik
+const font = Roboto({
+  weight: ["400", "500"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "block",
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <TransactionContext.Provider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        disableTransitionOnChange
-      >
-        <Meta />
-        <TransactionModal />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </TransactionContext.Provider>
+    <div className={font.className}>
+      <TransactionContext.Provider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          disableTransitionOnChange
+        >
+          <Meta />
+          <TransactionModal />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </TransactionContext.Provider>
+    </div>
   );
 };
 
