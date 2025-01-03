558354be6d4f0162ecf4af6110123e527fb345b5
walterdiazpalaestra <walterwalon@gmail.com>
2025-01-02 18:52:23 -0600

    + Test open Gmail from PWA | 5

--- components/Transaction/Modal/index.tsx
+++ components/Transaction/Modal/index.tsx
@@ -320,41 +320,31 @@ const TransactionModal = () => {
       {fromTransaction && (
         <div>
           {fromTransaction.id?.includes("@") && (
-            <>
-              <button
-                type="button"
-                className="flex text-gray-400 hover:text-gray-500 items-center justify-center mb-4 w-full rounded-md bg-gray-200 py-1.5 hover:bg-gray-300"
-                onClick={() => {
-                  /* const ref = `https://mail.google.com/mail/u/0/#search/rfc822msgid:${fromTransaction.id
+            <button
+              type="button"
+              className="flex text-gray-400 hover:text-gray-500 items-center justify-center mb-4 w-full rounded-md bg-gray-200 py-1.5 hover:bg-gray-300"
+              onClick={() => {
+                const isMobile =
+                  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                    navigator.userAgent
+                  );
+                const search = `rfc822msgid:${fromTransaction.id
                   .split("@")
                   .reverse()
-                  .join("@")}`; */
-                  const mailRef = `googlegmail:///search/rfc822msgid:${fromTransaction.id
-                    .split("@")
-                    .reverse()
-                    .join("@")}`;
-                  window.location.href = mailRef;
-                }}
-              >
-                <ArrowTopRightOnSquareIcon className="mr-2 h-4 w-4" />
-                Ver en Gmail
-              </button>
-              <div>
-                <input type="text" name="tempgmaillink" />
-                <button
-                  type="button"
-                  onClick={() => {
-                    const link = document.querySelector(
-                      "input[name=tempgmaillink]"
-                    ) as HTMLInputElement;
-                    alert(link.value);
-                    window.location.href = link.value;
-                  }}
-                >
-                  OpenTemp
-                </button>
-              </div>
-            </>
+                  .join("@")}`;
+                if (!isMobile)
+                  return window.open(
+                    `https://mail.google.com/mail/u/0/#search/${search}`,
+                    "_blank"
+                  );
+                navigator.clipboard
+                  .writeText(search)
+                  .then(() => (window.location.href = "googlegmail:///"));
+              }}
+            >
+              <ArrowTopRightOnSquareIcon className="mr-2 h-4 w-4" />
+              Ver en Gmail
+            </button>
           )}
           <button
             disabled={!!isMutatingTransaction}
